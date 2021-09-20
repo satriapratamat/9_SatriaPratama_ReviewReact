3270db40c89684804774594e9eddf93627da007e
Satria Pratama <satriapratamat7@gmail.com>
2021-09-20 12:33:59 +0700

    News page fix

--- src/component/NewsPage.js
+++ src/component/NewsPage.js
@@ -5,56 +5,60 @@ import classes from "./css/experience.css";
 import Loading from "./printilan/berita/Loading";
 
 function News() {
-  const [news, setNews] = useState([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+    const [news, setNews] = useState([]);
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://newsapi.org/v2/everything?q=tesla&from=2021-08-19&sortBy=publishedAt&apiKey=54554e65aa2c42d694bee8c4683fcc95"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setNews(data.articles);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setError(err.message);
-        setIsLoading(false);
-      });
-  }, []);
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(
+                "https://newsapi.org/v2/everything?q=tesla&from=2021-08-19&sortBy=publishedAt&apiKey=54554e65aa2c42d694bee8c4683fcc95"
+            )
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setNews(data.articles);
+                setIsLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
+                setError(err.message);
+                setIsLoading(false);
+            });
+    }, []);
 
-  return (
-    <div className={classes.bgImg}>
-      <Header />
-      <h3 className={classes.title}>News today</h3>
-      <div className={classes.container}>
-        {error && <p className={classes.error}>{error}</p>}
-        {isLoading &&
-          [1, 1, 1, 1].map((el, idx) => {
-            return <Loading key={idx} />;
-          })}
-        {!error && !isLoading && (
-          <ul>
-            {news.map((e, idx) => {
-              return (
-                <New
-                  key={idx}
-                  title={e.title}
-                  author={e.author}
-                  image={e.urlToImage}
-                  loading={isLoading}
-                />
-              );
-            })}
-          </ul>
-        )}
-      </div>
-    </div>
-  );
-}
+    return ( <
+        >
+        <
+        Header / >
+        <
+        h3 className = { classes.title } > News today < /h3> <
+        div className = { classes.container } > {
+            error && < p className = { classes.error } > { error } < /p>} {
+                isLoading && [1, 1, 1, 1].map((el, idx) => {
+                    return <Loading key = { idx }
+                    />;
+                })
+            } {
+                !error && !isLoading && ( <
+                    ul > {
+                        news.map((e, idx) => {
+                            return ( <
+                                New key = { idx }
+                                title = { e.title }
+                                author = { e.author }
+                                image = { e.urlToImage }
+                                loading = { isLoading }
+                                />
+                            );
+                        })
+                    } <
+                    /ul>
+                )
+            } <
+            /div> <
+            />
+        );
+    }
 
-export default News;
+    export default News;
